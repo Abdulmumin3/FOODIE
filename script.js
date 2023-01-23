908edc888237bf573fa8895f1320faeaad3eb39b
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-01-23 08:36:09 +0100

    made changes to the underlying problem sir

--- script.js
+++ script.js
@@ -24,18 +24,52 @@
 // for (let i = 0; i < showMore.length; i++) {
 //   showLess[i].addEventListener("click", closeContent[i]);
 // }
-const sections = document.querySelectorAll(".section");
-for (let i = 0; i < sections.length; i++) {
-  const section = sections[i];
-  const showMore = section.querySelector(".show-more");
-  const showLess = section.querySelector(".show-less");
-  const content = section.querySelector(".content");
-  showMore.addEventListener("click", () => show(content));
-  showLess.addEventListener("click", () => hide(content));
+// const sections = document.querySelectorAll(".section");
+// for (let i = 0; i < sections.length; i++) {
+//   const section = sections[i];
+//   const showMore = section.querySelector(".show-more");
+//   const showLess = section.querySelector(".show-less");
+//   const content = section.querySelector(".content");
+//   showMore.addEventListener("click", () => show(content));
+//   showLess.addEventListener("click", () => hide(content));
+// }
+// function show(el) {
+//   el.classList.remove("hidden");
+// }
+// function hide(el) {
+//   el.classList.add("hidden");
+// }
+
+// started from here
+// this is using the forEach loop
+const sections = document.querySelectorAll(".content");
+const showMore = document.querySelectorAll(".show-more");
+const showLess = document.querySelectorAll(".show-less");
+
+// showMore.forEach((btn, i) => {
+//   btn.addEventListener("click", () => {
+//     console.log(sections[i]);
+//     sections[i].classList.remove("hidden");
+//   });
+// });
+
+// showLess.forEach((btn, i) => {
+//   btn.addEventListener("click", () => {
+//     sections[i].classList.add("hidden");
+//   });
+// });
+
+// using the normal for loop
+for (let i = 0; i < showMore.length; i++) {
+  showMore[i].addEventListener("click", () => {
+    sections[i].classList.remove("hidden");
+  });
 }
-function show(el) {
-  el.classList.remove("hidden");
+
+for (let i = 0; i < showLess.length; i++) {
+  showLess[i].addEventListener("click", () => {
+    sections[i].classList.add("hidden");
+  });
 }
-function hide(el) {
-  el.classList.add("hidden");
-}
+
+// what this means is that, since all the buttons and the contents are the same nodelist length, then we can access a particular section based on the current index of the button that was clicked. You can console.log() things to see the way it happens.
